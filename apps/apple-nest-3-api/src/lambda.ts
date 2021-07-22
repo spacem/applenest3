60f49c8e8569b7fdeb06a3f8d71d217d0d94fa26
spacem <spacem>
2021-07-22 21:50:29 +1000

    try to fix netlify

--- apps/apple-nest-3-api/src/lambda.ts
+++ apps/apple-nest-3-api/src/lambda.ts
@@ -2,17 +2,29 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import serverlessExpress = require('@vendia/serverless-express');
 
-let server;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.init();
 
   const expressApp = app.getHttpAdapter().getInstance();
-  return serverlessExpress.configure({ app: expressApp });
+  return serverlessExpress.configure({
+    eventSource: {
+      getRequest: ({ event }) => {
+        return {
+          method: event.httpMethod,
+          path: event.path,
+          headers: event.headers
+        };
+      }
+    },
+    app: expressApp
+  });
 }
 
+// let server;
 export const handler = async (event, context, callback) => {
-  server = server ?? (await bootstrap());
+  const server = await bootstrap()
+  // server = server ?? (await bootstrap());
   return server(event, context, callback);
 };
