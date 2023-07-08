6f0f48bd55cd5e5e79aca201164f495955c46900
spacem <spacem>
2023-07-08 21:06:49 +1000

    Fix back to town

--- apps/apple-nest-3/src/app/places/places.ts
+++ apps/apple-nest-3/src/app/places/places.ts
@@ -144,10 +144,10 @@ export const places: {[key:string]: PlaceConfig} = {
         level: 3,
       },
       {
-        title: 'Go Back To Farm',
+        title: 'Go Back To Town',
         action: 'nav',
-        param: 'farm',
-        icon: 'farm.jpg',
+        param: 'town',
+        icon: 'town.jpg',
       },
     ]
   },
@@ -170,10 +170,10 @@ export const places: {[key:string]: PlaceConfig} = {
         level: 3,
       },
       {
-        title: 'Go Back To Town',
+        title: 'Go Back To Farm',
         action: 'nav',
-        param: 'town',
-        icon: 'town.jpg',
+        param: 'farm',
+        icon: 'farm.jpg',
       },
     ]
   },
